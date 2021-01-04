a805488c3c1fb2f740a8cdf2200fa30a17082914
sunil880018 <sunil880018@gmail.com>
2021-01-04 13:45:47 +0530

    change

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ if(process.env.NODE_ENV === "production"){
     app.use(express.static(path.join(__dirname,"../gym/build")));
 
     app.get('*',(req,res) =>{
-        res.sendFile(path.resolve(__dirname,"../gym/build/index.html"))
+        res.sendFile(path.resolve(__dirname,'gym','build','index.html'));
     })
 }else{
     app.get('/',(req,res) =>{
