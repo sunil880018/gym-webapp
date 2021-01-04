8dabd4775cc83f2870eee41cce6085a9a5f83613
sunil880018 <sunil880018@gmail.com>
2021-01-04 10:50:58 +0530

    package json change

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const dotenv = require('dotenv');
+const path = require('path')
 const connectDB = require("./config/connectionDB");
 const User = require('./models/Register');
 connectDB()
@@ -10,6 +11,13 @@ const PORT = process.env.PORT||5000
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('gym/build'));
+
+    app.get('*',(req,res) =>{
+        res.sendFile(path.resolve(__dirname,'gym','build','index.html'));
+    })
+}
 
 
 app.post("/register",async(req,res) =>{
