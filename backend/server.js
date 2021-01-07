920b2cf5c8bde1923a852440761dcb6f1030befe
sunil880018 <sunil880018@gmail.com>
2021-01-07 12:57:37 +0530

    initial

--- backend/server.js
+++ backend/server.js
@@ -11,20 +11,6 @@ const PORT = process.env.PORT||5000
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
-
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname,"../gym/build")));
-
-    app.get('*',(req,res) =>{
-        res.sendFile(path.join(__dirname,'gym','build','index.html'));
-    })
-}else{
-    app.get('/',(req,res) =>{
-        res.send('running')
-    })
-}
-
-
 app.post("/register",async(req,res) =>{
     try{
         const reg = new User({
